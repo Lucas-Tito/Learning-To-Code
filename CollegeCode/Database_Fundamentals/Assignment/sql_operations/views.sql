b554518edd24dea67d6127b2d6d7c6e89a383802
Lucas-Tito <lucassga500@gmail.com>
2023-07-10 16:56:42 -0300

    finished project

--- CollegeCode/Database_Fundamentals/Assignment/sql_operations/views.sql
+++ CollegeCode/Database_Fundamentals/Assignment/sql_operations/views.sql
@@ -1,11 +1,11 @@
---ideia de view -> top 3 clientes que mais gastaram no ano de 2023
-CREATE VIEW top3_spenders AS
+--ideia de view -> top 3 clientes que menos gastaram no ano de 2023
+CREATE VIEW top3_nonspenders AS
 SELECT cliente.rg_cliente, cliente.nome, 
     (SELECT SUM(quarto.preco) FROM quarto 
     INNER JOIN alocacao_quarto ON quarto.id_quarto = alocacao_quarto.id_quarto
     WHERE alocacao_quarto.rg_cliente = cliente.rg_cliente
     AND alocacao_quarto.data_alocacao_inicio BETWEEN '2023-01-01' AND '2023-12-31') AS total_gasto
 FROM cliente
-ORDER BY total_gasto DESC
+ORDER BY total_gasto ASC
 LIMIT 3;
 
